d928406b6460cdb3c76ea2e381a829dbaea0877c
badrulhusain <badru773dhdc@gmail.com>
2025-08-21 18:25:06 +0530

    a

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -9,3 +9,21 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
 });
 
+interface CloudinaryUploadResult {
+    public_id: string;
+    [key: string]: any
+}
+export async function POST(req:NextRequest){
+    const {userId}:any =auth();
+    
+    if(!userId){
+        return NextResponse.json({error:"unauthorized"},{status:401})
+    }
+    try {
+        const formadata= await req.formData() ;
+        const file = formadata.get('file') as File || null 
+        
+    } catch (error) {
+        
+    }
+}
