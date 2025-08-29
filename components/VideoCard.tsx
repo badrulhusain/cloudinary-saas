b1eb1c095c2a5c33d97147fa4958cb7e02155bd0
badrul husain <badru773dhdc@gmail.com>
2025-08-29 11:39:47 +0530

    s

---
+++ components/VideoCard.tsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect,useCallback}    from 'react'
+import {getCldImageUrl,getCldVideoUrl} from "next-cloudinary"
+
+function VideoCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default VideoCard
